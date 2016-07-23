eac9db9e413089a2b686e54bf6ff9984e54804e7
John.Koepi <john.koepi@gmail.com>
2016-07-23 16:31:56 +0200

    fix #2883, #2857 pipe fds leak when fork() failed on bg aof rw

--- redis-4.0-rc2/src/aof.c
+++ redis-4.0-rc2/src/aof.c
@@ -1372,6 +1372,7 @@ int rewriteAppendOnlyFileBackground(void) {
             serverLog(LL_WARNING,
                 "Can't rewrite append only file in background: fork: %s",
                 strerror(errno));
+            aofClosePipes();
             return C_ERR;
         }
         serverLog(LL_NOTICE,
