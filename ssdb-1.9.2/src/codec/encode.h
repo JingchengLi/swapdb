5fac562d703bdedf7bc3f89700c10c87ef9b92ba
luoming <luoming1224@126.com>
2016-11-03 10:37:34 +0800

    add encode meta value

--- ssdb-1.9.2/src/codec/encode.h
+++ ssdb-1.9.2/src/codec/encode.h
@@ -9,8 +9,6 @@
 #include <string>
 using namespace std;
 
-#define ZSET_SCORE_SHIFT 1000000000000000000LL
-
 string encode_meta_key(const string& key);
 
 static string encode_key_internal(const string& key, const string& field, uint16_t version);
@@ -26,5 +24,21 @@ static uint64_t encodeScore(const double score);
 string encode_zscore_key(const string& key, const string& member, double score, uint16_t version);
 
 string encode_list_key(const string& key, uint64_t seq, uint16_t version);
+
+/*
+ * encode meta value
+ */
+
+string encode_kv_val(const string& val);
+
+static string encode_meta_val_internal(const char type, uint64_t length, uint16_t version, char del);
+
+string encode_hash_meta_val(uint64_t length, uint16_t version = 0, char del = KEY_ENABLED_MASK);
+
+string encode_set_meta_val(uint64_t length, uint16_t version = 0, char del = KEY_ENABLED_MASK);
+
+string encode_zset_meta_val(uint64_t length, uint16_t version = 0, char del = KEY_ENABLED_MASK);
+
+string encode_list_meta_val(uint64_t length, uint64_t left, uint64_t right, uint16_t version = 0, char del = KEY_ENABLED_MASK);
 
 #endif //SSDB_ENCODE_H
