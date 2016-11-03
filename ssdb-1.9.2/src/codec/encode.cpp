a731b2fc931dd10880e158131145bab0c0349d6a
luoming <luoming1224@126.com>
2016-11-03 14:39:15 +0800

    .

--- ssdb-1.9.2/src/codec/encode.cpp
+++ ssdb-1.9.2/src/codec/encode.cpp
@@ -6,12 +6,11 @@
 
 string encode_meta_key(const string& key){
     string buf;
-    unsigned char p[3] = {0};
 
     buf.append(1, 'M');
     uint16_t slot = (uint16_t)keyHashSlot(key.data(), (int)key.size());
-    zipSaveInteger(p, slot, ZIP_INT_16B); // 2 Bytes
-    buf.append((const char*)p, 2);
+    slot = htobe16(slot);
+    buf.append((char *)&slot, sizeof(uint16_t));
 
     buf.append(key);
 
@@ -20,14 +19,13 @@ string encode_meta_key(const string& key){
 
 string encode_key_internal(const string& key, const string& field, uint16_t version){
     string buf;
-    unsigned char p[3] = {0};
 
     buf.append(1, 'S');
-    zipSaveInteger(p, version, ZIP_INT_16B); // 2 Bytes
-    buf.append((const char*)p, 2);
+    version = htobe16(version);
+    buf.append((char *)&version, sizeof(uint16_t));
 
-    zipSaveInteger(p, (int64_t)key.size(), ZIP_INT_16B); // 2 Bytes
-    buf.append((const char*)p, 2);
+    uint16_t len = htobe16((uint16_t)key.size());
+    buf.append((char *)&len, sizeof(uint16_t));
     buf.append(key);
 
     buf.append(field);
@@ -59,19 +57,18 @@ inline uint64_t encodeScore(const double score) {
 
 string encode_zscore_key(const string& key, const string& member, double score, uint16_t version){
     string buf;
-    unsigned char p[3] = {0};
 
     buf.append(1, 'S');
-    zipSaveInteger(p, version, ZIP_INT_16B); // 2 Bytes
-    buf.append((const char*)p, 2);
+    version = htobe16(version);
+    buf.append((char *)&version, sizeof(uint16_t));
 
-    zipSaveInteger(p, (int64_t)key.size(), ZIP_INT_16B); // 2 Bytes
-    buf.append((const char*)p, 2);
+    uint16_t len = htobe16((uint16_t)key.size());
+    buf.append((char *)&len, sizeof(uint16_t));
     buf.append(key);
 
     uint64_t new_score = encodeScore(score);
     new_score = htobe64(new_score);
-    zipSaveInteger(p, new_score, ZIP_INT_64B); // 8 Byte
+    buf.append((char *)&new_score, sizeof(uint64_t));
 
     buf.append(member);
 
@@ -80,17 +77,15 @@ string encode_zscore_key(const string& key, const string& member, double score, 
 
 string encode_list_key(const string& key, uint64_t seq, uint16_t version){
     string buf;
-    unsigned char p[3] = {0};
-    unsigned char seq_buf[9] = {0};
 
     buf.append(1, 'S');
-    zipSaveInteger(p, version, ZIP_INT_16B); // 2 Bytes
-    buf.append((const char*)p, 2);
+    version = htobe16(version);
+    buf.append((char *)&version, sizeof(uint16_t));
 
     buf.append(key);
 
-    zipSaveInteger(seq_buf, seq, ZIP_INT_64B);
-    buf.append((const char*)seq_buf, 8);
+    seq = htobe64(seq);
+    buf.append((char *)&seq, sizeof(uint64_t));
 
     return buf;
 }
@@ -104,17 +99,15 @@ string encode_kv_val(const string& val){
 
 string encode_meta_val_internal(const char type, uint64_t length, uint16_t version, char del){
     string buf;
-    unsigned char p[3] = {0};
-    unsigned char len_buf[9] = {0};
 
     buf.append(1, type);
-    zipSaveInteger(p, version, ZIP_INT_16B); // 2 Bytes
-    buf.append((const char*)p, 2);
+    version = htobe16(version);
+    buf.append((char *)&version, sizeof(uint16_t));
 
     buf.append(1, del);
 
-    zipSaveInteger(len_buf, length, ZIP_INT_64B);
-    buf.append((const char*)len_buf, 8);
+    length = htobe64(length);
+    buf.append((char *)&length, sizeof(uint64_t));
 
     return buf;
 }
@@ -133,23 +126,21 @@ string encode_zset_meta_val(uint64_t length, uint16_t version, char del){
 
 string encode_list_meta_val(uint64_t length, uint64_t left, uint64_t right, uint16_t version, char del){
     string buf;
-    unsigned char p[3] = {0};
-    unsigned char u64[9] = {0};
 
     buf.append(1, DataType::LSZIE);
-    zipSaveInteger(p, version, ZIP_INT_16B); // 2 Bytes
-    buf.append((const char*)p, 2);
+    version = htobe16(version);
+    buf.append((char *)&version, sizeof(uint16_t));
 
     buf.append(1, del);
 
-    zipSaveInteger(u64, length, ZIP_INT_64B);
-    buf.append((const char*)u64, 8);
+    length = htobe64(length);
+    buf.append((char *)&length, sizeof(uint64_t));
 
-    zipSaveInteger(u64, left, ZIP_INT_64B);
-    buf.append((const char*)u64, 8);
+    left = htobe64(left);
+    buf.append((char *)&left, sizeof(uint64_t));
 
-    zipSaveInteger(u64, right, ZIP_INT_64B);
-    buf.append((const char*)u64, 8);
+    right = htobe64(right);
+    buf.append((char *)&right, sizeof(uint64_t));
 
     return buf;
 }
