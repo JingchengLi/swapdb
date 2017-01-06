a2f5cb99eac8ad9d1c742a9e17cfc54cca962d5d
zhaojun <motablow@gmail.com>
2017-01-06 17:15:44 +0800

    [Test] Add redis tcl tests for ssdb.

---
+++ ssdb-1.9.2/tests/qa/integration/tcl/ssdb/dump.tcl
@@ -0,0 +1,244 @@
+#current need to start redis and ssdb manually, wait Dev can start ssdb when redis start
+start_server {tags {"redis-ssdb"}} {
+    set ssdb [redis 127.0.0.1 8888]
+    set redis [redis 127.0.0.1 6379]
+
+#string type
+    foreach valtype {string-encoded integer-encoded mix-encoded} {
+        if {$valtype == "string-encoded"} {
+            set bar bar
+        } elseif {$valtype == "integer-encoded"} {
+            set bar 123
+        } elseif {$valtype == "mix-encoded"} {
+            set bar bar123
+        }
+
+        test "string $valtype Be Hot ssdb dump/redis restore" {
+            $redis del foo
+            $ssdb set foo $bar
+            set ssdbEncoded [$ssdb dump foo]
+            $ssdb del foo
+            $redis restore foo 0 $ssdbEncoded
+            assert_equal $bar [ $redis get foo ]
+        }
+
+        test "string $valtype Be Cold redis dump/ssdb restore" {
+            $redis del foo
+            $redis set foo $bar
+            set redisEncoded [$redis dump foo]
+            $redis del foo
+            $ssdb restore foo 0 $redisEncoded
+            assert_equal $bar [ $ssdb get foo ]
+        }
+    }
+
+#hash type
+    foreach encoding { ziplist hashtable} {
+        if {$encoding == "ziplist"} {
+            set num 8
+        } elseif {$encoding == "hashtable"} {
+            set num 1024
+        }
+
+        test "hash ($encoding) creation" {
+            $redis del myhash
+            array set myhash {}
+            unset myhash
+            for {set i 0} {$i < $num} {incr i} {
+                set key __avoid_collisions__[randstring 0 8 alpha]
+                set val __avoid_collisions__[randstring 0 8 alpha]
+                if {[info exists myhash($key)]} {
+                    incr i -1
+                    continue
+                }
+                $redis hset myhash $key $val
+                set myhash($key) $val
+            }
+            assert {[$redis object encoding myhash] eq $encoding}
+        }
+
+        test "hash ($encoding) Be Cold redis dump/ssdb restore" {
+            set redisEncoded [$redis dump myhash]
+
+            $redis del myhash
+            $ssdb restore myhash 0 $redisEncoded
+
+            set err {}
+            foreach k [array names myhash *] {
+                if {$myhash($k) ne [$ssdb hget myhash $k]} {
+                    set err "$myhash($k) != [$ssdb hget myhash $k]"
+                    break
+                }
+            }
+            set _ $err
+        } {}
+
+        test "hash ($encoding) Be Hot ssdb dump/redis restore" {
+            set ssdbEncoded [$ssdb dump myhash]
+
+            $ssdb del myhash
+
+            $redis restore myhash 0 $ssdbEncoded
+            set err {}
+            foreach k [array names myhash *] {
+                if {$myhash($k) ne [$redis hget myhash $k]} {
+                    set err "$myhash($k) != [$redis hget myhash $k]"
+                    break
+                }
+            }
+            $redis del myhash
+            set _ $err
+        } {}
+
+        test {RESTORE can detect a syntax error for unrecongized options} {
+            catch {$ssdb restore myhash 0 $redisEncoded invalid-option} e
+            set e
+        } {*ERR*}
+    }
+# set type
+    foreach valtype {string-encoded mix-encoded integer-encoded overflownumbers} \
+        encoding { hashtable hashtable intset hashtable } {
+        if {$valtype == "string-encoded"} {
+            set list [list a b c d e f g]
+        } elseif {$valtype == "mix-encoded"} {
+            set list [list 1 2 3 4 5 6 7 a b c d e f g]
+        } elseif {$valtype == "integer-encoded"} {
+            set list [list 1 2 3 4 5 6 7]
+        } elseif {$valtype == "overflownumbers"} {
+            set list {}
+            for {set i 0} {$i <=  512} {incr i} { 
+                lappend list $i
+            }
+        }
+
+        test "set ($encoding) $valtype creation" {
+            $redis del myset
+            foreach i $list {
+                $redis sadd myset $i
+            }
+            assert {[$redis object encoding myset] eq $encoding}
+        }
+
+        test "set ($encoding) $valtype Be Cold redis dump/ssdb restore" {
+            set redisEncoded [$redis dump myset]
+            $redis del myset
+            $ssdb restore myset 0 $redisEncoded
+
+            assert_equal [lsort $list] [lsort [$ssdb smembers myset]]
+        }
+
+        test "set ($encoding) $valtype Be Hot ssdb dump/redis restore" {
+            set ssdbEncoded [$ssdb dump myset]
+            $ssdb del myset
+            $redis restore myset 0 $ssdbEncoded
+
+            assert_equal [lsort $list] [lsort [$redis smembers myset]]
+            $redis del myset
+        }
+
+        test {RESTORE (SSDB) can set an arbitrary expire to the materialized key} {
+            $ssdb del foottl
+            $ssdb restore foottl 5000 $redisEncoded
+            set ttl [$ssdb pttl foottl]
+            assert {$ttl >= 3000 && $ttl <= 5000}
+        }
+        $redis del foottl
+    }
+
+#zset type
+    foreach encoding {ziplist skiplist} {
+        if {$encoding == "ziplist"} {
+            $redis config set zset-max-ziplist-entries 128
+            $redis config set zset-max-ziplist-value 64
+        } elseif {$encoding == "skiplist"} {
+            $redis config set zset-max-ziplist-entries 1
+            $redis config set zset-max-ziplist-value 1
+        }
+
+        test "zset ($encoding) creation" {
+            $redis del myzset
+            set list {}
+            foreach i {1 2 3 4} j {a b c d} {
+                $redis zadd myzset $i $j
+                lappend list $j $i
+            }
+            assert {[$redis object encoding myzset] eq $encoding}
+        }
+
+        test "zset ($encoding) Be Cold redis dump/ssdb restore" {
+            set redisEncoded [$redis dump myzset]
+            $redis del myzset
+            $ssdb restore myzset 0 $redisEncoded
+
+            assert_equal $list [$ssdb zrange myzset 0 -1 withscores]
+        }
+
+        test "zset ($encoding) Be Hot ssdb dump/redis restore" {
+            set ssdbEncoded [$ssdb dump myzset]
+            $ssdb del myzset
+            $redis restore myzset 0 $ssdbEncoded
+
+            assert_equal $list [$redis zrange myzset 0 -1 withscores]
+            $redis del myzset
+        }
+
+        test {RESTORE (SSDB) can set an expire that overflows a 32 bit integer} {
+            $ssdb del foottl
+            $ssdb restore foottl 2569591501 $redisEncoded
+            set ttl [$ssdb pttl foottl]
+            assert {$ttl >= 2569591501-3000 && $ttl <= 2569591501}
+        }
+        $redis del foottl
+    }
+
+#list type
+    foreach valtype {string-encoded integer-encoded mix-encoded} {
+        if {$valtype == "string-encoded"} {
+            set list [list a b c d e f g]
+        } elseif {$valtype == "integer-encoded"} {
+            set list [list 1 2 3 4 5 6 7]
+        } elseif {$valtype == "mix-encoded"} {
+            set list [list 1 2 3 4 5 6 7 a b c d e f g]
+        }
+        
+        test "list (quicklist) $valtype creation" {
+            $redis del mylist
+            foreach i $list {
+                $redis lpush mylist $i
+            }
+            assert {[$redis object encoding mylist] eq {quicklist}}
+        }
+
+        test "list (quicklist) $valtype Be Cold redis dump/ssdb restore" {
+            set redisEncoded [$redis dump mylist]
+            $redis del mylist
+            $ssdb restore mylist 0 $redisEncoded
+
+            assert_equal [lsort $list] [lsort [$ssdb lrange mylist 0 -1]]
+        }
+
+        test "list (quicklist) $valtype Be Hot ssdb dump/redis restore" {
+            set ssdbEncoded [$ssdb dump mylist]
+            $ssdb del mylist
+            $redis restore mylist 0 $ssdbEncoded
+
+            assert_equal [lsort $list] [lsort [$redis lrange mylist 0 -1]]
+            $redis del mylist
+        }
+
+        test {RESTORE (SSDB) returns an error of the key already exists} {
+            $ssdb del foobusy
+            $ssdb set foobusy barbusy
+            set e {}
+            catch { $ssdb restore foobusy 0 $redisEncoded } e
+            list [set e] [ $ssdb get foobusy ]
+        } {*ERR* barbusy}
+
+        test {RESTORE (SSDB) can overwrite an existing key with REPLACE} {
+            $ssdb restore foobusy 0 $redisEncoded replace
+            assert_equal [lsort $list] [lsort [$ssdb lrange foobusy 0 -1]]
+        }
+        #TODO redis cannot del key stored in ssdb currently.
+        $redis del foobusy
+    }
+}
---
+++ ssdb-1.9.2/tests/qa/integration/tcl/ssdb/list.tcl
@@ -0,0 +1,247 @@
+source "./ssdb/init_ssdb.tcl"
+start_server {
+    tags {"ssdb"}
+    overrides {
+        "list-max-ziplist-size" 5
+    }
+} {
+    source "ssdb/list-common.tcl"
+
+    test {LPUSH, RPUSH, LLENGTH, LINDEX, LPOP - ziplist} {
+        # first lpush then rpush
+        r del myziplist1
+        assert_equal 1 [r lpush myziplist1 aa]
+        assert_equal 2 [r rpush myziplist1 bb]
+        assert_equal 3 [r rpush myziplist1 cc]
+        assert_equal 3 [r llen myziplist1]
+        assert_equal aa [r lindex myziplist1 0]
+        assert_equal bb [r lindex myziplist1 1]
+        assert_equal cc [r lindex myziplist1 2]
+        assert_equal {} [r lindex myziplist2 3]
+        assert_equal cc [r rpop myziplist1]
+        assert_equal aa [r lpop myziplist1]
+        assert_encoding quicklist myziplist1
+
+        # first rpush then lpush
+        r del myziplist2
+        assert_equal 1 [r rpush myziplist2 a]
+        assert_equal 2 [r lpush myziplist2 b]
+        assert_equal 3 [r lpush myziplist2 c]
+        assert_equal 3 [r llen myziplist2]
+        assert_equal c [r lindex myziplist2 0]
+        assert_equal b [r lindex myziplist2 1]
+        assert_equal a [r lindex myziplist2 2]
+        assert_equal {} [r lindex myziplist2 3]
+        assert_equal a [r rpop myziplist2]
+        assert_equal c [r lpop myziplist2]
+        assert_encoding quicklist myziplist2
+    }
+
+    test {LPUSH, RPUSH, LLENGTH, LINDEX, LPOP - regular list} {
+        # first lpush then rpush
+        r del mylist1
+        assert_equal 1 [r lpush mylist1 $largevalue(linkedlist)]
+        assert_encoding quicklist mylist1
+        assert_equal 2 [r rpush mylist1 b]
+        assert_equal 3 [r rpush mylist1 c]
+        assert_equal 3 [r llen mylist1]
+        assert_equal $largevalue(linkedlist) [r lindex mylist1 0]
+        assert_equal b [r lindex mylist1 1]
+        assert_equal c [r lindex mylist1 2]
+        assert_equal {} [r lindex mylist1 3]
+        assert_equal c [r rpop mylist1]
+        assert_equal $largevalue(linkedlist) [r lpop mylist1]
+
+        # first rpush then lpush
+        r del mylist2
+        assert_equal 1 [r rpush mylist2 $largevalue(linkedlist)]
+        assert_encoding quicklist mylist2
+        assert_equal 2 [r lpush mylist2 b]
+        assert_equal 3 [r lpush mylist2 c]
+        assert_equal 3 [r llen mylist2]
+        assert_equal c [r lindex mylist2 0]
+        assert_equal b [r lindex mylist2 1]
+        assert_equal $largevalue(linkedlist) [r lindex mylist2 2]
+        assert_equal {} [r lindex mylist2 3]
+        assert_equal $largevalue(linkedlist) [r rpop mylist2]
+        assert_equal c [r lpop mylist2]
+    }
+
+    test {R/LPOP against empty list} {
+        r lpop non-existing-list
+    } {}
+
+    test {Variadic RPUSH/LPUSH} {
+        r del mylist
+        assert_equal 4 [r lpush mylist a b c d]
+        assert_equal 8 [r rpush mylist 0 1 2 3]
+        assert_equal {d c b a 0 1 2 3} [r lrange mylist 0 -1]
+    }
+
+    test {DEL a list} {
+        assert_equal 1 [r del mylist2]
+        assert_equal 0 [r exists mylist2]
+        assert_equal 0 [r llen mylist2]
+    }
+
+    proc create_list {key entries} {
+        r del $key
+        foreach entry $entries { r rpush $key $entry }
+        assert_encoding quicklist $key
+    }
+
+
+
+    foreach {type num} {quicklist 250 quicklist 500} {
+        proc check_numbered_list_consistency {key} {
+            set len [r llen $key]
+            for {set i 0} {$i < $len} {incr i} {
+                assert_equal $i [r lindex $key $i]
+                assert_equal [expr $len-1-$i] [r lindex $key [expr (-$i)-1]]
+            }
+        }
+
+        proc check_random_access_consistency {key} {
+            set len [r llen $key]
+            for {set i 0} {$i < $len} {incr i} {
+                set rint [expr int(rand()*$len)]
+                assert_equal $rint [r lindex $key $rint]
+                assert_equal [expr $len-1-$rint] [r lindex $key [expr (-$rint)-1]]
+            }
+        }
+
+        test "LINDEX consistency test - $type" {
+            r del mylist
+            for {set i 0} {$i < $num} {incr i} {
+                r rpush mylist $i
+            }
+            assert_encoding $type mylist
+            check_numbered_list_consistency mylist
+        }
+
+        test "LINDEX random access - $type" {
+            assert_encoding $type mylist
+            check_random_access_consistency mylist
+        }
+    }
+
+    test {LLEN against non-list value error} {
+        r del mylist
+        r set mylist foobar
+        assert_error ERR* {r llen mylist}
+    }
+
+    test {LLEN against non existing key} {
+        assert_equal 0 [r llen not-a-key]
+    }
+
+    test {LINDEX against non-list value error} {
+        assert_error ERR* {r lindex mylist 0}
+    }
+
+    test {LINDEX against non existing key} {
+        assert_equal "" [r lindex not-a-key 10]
+    }
+
+    test {LPUSH against non-list value error} {
+        assert_error ERR* {r lpush mylist 0}
+    }
+
+    test {RPUSH against non-list value error} {
+        assert_error ERR* {r rpush mylist 0}
+    }
+
+    foreach {type large} [array get largevalue] {
+        test "Basic LPOP/RPOP - $type" {
+            create_list mylist "$large 1 2"
+            assert_equal $large [r lpop mylist]
+            assert_equal 2 [r rpop mylist]
+            assert_equal 1 [r lpop mylist]
+            assert_equal 0 [r llen mylist]
+
+            # pop on empty list
+            assert_equal {} [r lpop mylist]
+            assert_equal {} [r rpop mylist]
+        }
+    }
+
+    test {LPOP/RPOP against non list value} {
+        r set notalist foo
+        assert_error ERR* {r lpop notalist}
+        assert_error ERR* {r rpop notalist}
+    }
+
+    foreach {type num} {quicklist 250 quicklist 500} {
+        test "Mass RPOP/LPOP - $type" {
+            r del mylist
+            set sum1 0
+            for {set i 0} {$i < $num} {incr i} {
+                r lpush mylist $i
+                incr sum1 $i
+            }
+            assert_encoding $type mylist
+            set sum2 0
+            for {set i 0} {$i < [expr $num/2]} {incr i} {
+                incr sum2 [r lpop mylist]
+                incr sum2 [r rpop mylist]
+            }
+            assert_equal $sum1 $sum2
+        }
+    }
+
+    foreach {type large} [array get largevalue] {
+        test "LRANGE basics - $type" {
+            create_list mylist "$large 1 2 3 4 5 6 7 8 9"
+            assert_equal {1 2 3 4 5 6 7 8} [r lrange mylist 1 -2]
+            assert_equal {7 8 9} [r lrange mylist -3 -1]
+            assert_equal {4} [r lrange mylist 4 4]
+        }
+
+        test "LRANGE inverted indexes - $type" {
+            create_list mylist "$large 1 2 3 4 5 6 7 8 9"
+
+            assert_error NoERR* {r lrange mylist 6 2}
+            # assert_equal {} [r lrange mylist 6 2]
+        }
+
+        test "LRANGE out of range indexes including the full list - $type" {
+            create_list mylist "$large 1 2 3"
+            assert_equal "$large 1 2 3" [r lrange mylist -1000 1000]
+        }
+
+        test "LRANGE out of range negative end index - $type" {
+            create_list mylist "$large 1 2 3"
+            assert_equal $large [r lrange mylist 0 -4]
+
+            assert_error NoERR* {r lrange mylist 0 -5}
+            # assert_equal {} [r lrange mylist 0 -5]
+        }
+    }
+
+    test {LRANGE against non existing key} {
+        assert_equal {} [r lrange nosuchkey 0 1]
+    }
+
+    foreach {type large} [array get largevalue] {
+        test "LSET - $type" {
+            create_list mylist "99 98 $large 96 95"
+            r lset mylist 1 foo
+            r lset mylist -1 bar
+            assert_equal "99 foo $large 96 bar" [r lrange mylist 0 -1]
+        }
+
+        test "LSET out of range index - $type" {
+            assert_error ERR* {r lset mylist 10 foo}
+        }
+    }
+
+    test {LSET against non existing key} {
+        assert_error ERR* {r lset nosuchkey 10 foo}
+    }
+
+    test {LSET against non list value} {
+        r set nolist foobar
+        assert_error ERR* {r lset nolist 0 foo}
+    }
+    r del mylist
+}
