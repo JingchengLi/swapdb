1bcd185f9557d93ddf1070a76ccc8fc5e602457d
antirez <antirez@gmail.com>
2017-01-18 17:05:10 +0100

    serverPanic(): allow printf() alike formatting.
    This is of great interest because allows us to print debugging
    informations that could be of useful when debugging, like in the
    following example:
    
        serverPanic("Unexpected encoding for object %d, %d",
            obj->type, obj->encoding);

--- redis-4.0-rc2/src/server.h
+++ redis-4.0-rc2/src/server.h
@@ -555,7 +555,7 @@ long long test_incr_id;
 /* We can print the stacktrace, so our assert is defined this way: */
 #define serverAssertWithInfo(_c,_o,_e) ((_e)?(void)0 : (_serverAssertWithInfo(_c,_o,#_e,__FILE__,__LINE__),_exit(1)))
 #define serverAssert(_e) ((_e)?(void)0 : (_serverAssert(#_e,__FILE__,__LINE__),_exit(1)))
-#define serverPanic(_e) _serverPanic(#_e,__FILE__,__LINE__),_exit(1)
+#define serverPanic(...) _serverPanic(__FILE__,__LINE__,__VA_ARGS__),_exit(1)
 
 /*-----------------------------------------------------------------------------
  * Data types
@@ -2366,7 +2366,7 @@ client* createReuseClient(client* reuse, int fd);
 /* Debugging stuff */
 void _serverAssertWithInfo(const client *c, const robj *o, const char *estr, const char *file, int line);
 void _serverAssert(const char *estr, const char *file, int line);
-void _serverPanic(const char *msg, const char *file, int line);
+void _serverPanic(const char *file, int line, const char *msg, ...);
 void bugReportStart(void);
 void serverLogObjectDebugInfo(const robj *o);
 void sigsegvHandler(int sig, siginfo_t *info, void *secret);
