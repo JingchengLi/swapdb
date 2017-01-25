8c25b3814daa1a003b90483bbfa352cfb23ec8d0
zhangtianshuo <zts1993@gmail.com>
2017-01-25 11:08:13 +0800

    fix build . 集成QA test , 支持测试中断点调试

--- ssdb-1.9.2/src/version.h
+++ ssdb-1.9.2/src/version.h
@@ -1,6 +1,15 @@
 #ifndef SSDB_DEPS_H
 #ifndef SSDB_VERSION
+#ifndef SSDB_DEPS_H
+#ifndef SSDB_VERSION
 #define SSDB_VERSION "1.9.4"
+#endif
+#endif
+#define SSDB_VERSION "1.9.4"
+#ifndef IOS
+#include <stdlib.h>
+#endif
+#include <jemalloc/jemalloc.h>
 #endif
 #endif
 #ifndef IOS
