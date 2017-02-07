7d676163ab2d37204edf8f320f53e79225bd6bf1
zhaojun <motablow@gmail.com>
2017-02-07 20:12:51 +0800

    [Test] add voilate keys dumpto ssdb case.

--- redis-4.0-rc2/tests/qa/integration/ssdb/communicate-base.tcl
+++ redis-4.0-rc2/tests/qa/integration/ssdb/communicate-base.tcl
@@ -18,7 +18,7 @@ start_server {tags {"ssdb"}} {
         set start [string first $s $info]
         set end [string first " " $info [expr $start+$len+1 ]]
         string range $info [expr $start+$len+1 ] $end-1
-    } 
+    }
 
     test "Ssdb is up" {
         $ssdb ping
@@ -29,105 +29,133 @@ start_server {tags {"ssdb"}} {
         #redis should have some flag/status to check this
     }
 
-    test "Hot key only store in redis" {
+    foreach ttl {0 1000} {
+        test "Hot key only store in redis with ttl($ttl)" {
+            $redis del foo
+
+            if {$ttl > 0} {
+                $redis setex foo $ttl bar
+            } else {
+                $redis set foo bar
+            }
+            list [$redis locatekey foo] [ $ssdb get foo ] [ $redis get foo ]
+        } {redis {} bar}
+
+        test "Key(become cold) move from redis to ssdb with ttl($ttl)" {
+            $redis dumptossdb foo
+            $redis set fooxxx barxxx
+
+            #wait key dumped to ssdb
+            wait_for_condition 100 1 {
+                [ $ssdb get foo ] eq {bar}
+            } else {
+                fail "key foo be dumptossdb failed"
+            }
+
+            $redis bgsave
+            #wait bgsave complete
+            after 200
+            #start a test redis-server not connect to ssdb.
+            set nossdbpid [exec ../../../src/redis-server $nossdbcfgfile > /dev/null &]
+            after 1000
+            set redis_no_ssdb [redis 127.0.0.1 6389]
+            assert_equal "barxxx" [$redis_no_ssdb get fooxxx] "fooxxx stored in redis"
+            list [$redis locatekey foo] [ $redis_no_ssdb get foo ] [ $ssdb get foo ]
+        } {ssdb {} bar}
+
+        test "Redis can read key stored in ssdb with ttl($ttl)" {
+            $redis get foo
+        } {bar}
+
+        test "GET Key(become hot) move from ssdb to redis with ttl($ttl)" {
+            list [$redis locatekey foo] [ $ssdb get foo ]
+        } {redis {}}
+
+        test "Key(become cold) move from redis to ssdb with ttl($ttl)" {
+            $redis dumptossdb foo
+
+            #wait key dumped to ssdb
+            wait_for_condition 100 1 {
+                [ $ssdb get foo ] eq {bar}
+            } else {
+                fail "key foo be dumptossdb failed"
+            }
+
+            $redis locatekey foo
+        } {ssdb}
+
+        test "SET Key(become hot) move from ssdb to redis with ttl($ttl)" {
+            if {$ttl > 0} {
+                $redis setex foo $ttl bar1
+            } else {
+                $redis set foo bar1
+            }
+            list [$redis locatekey foo] [$ssdb get foo] [$redis get foo]
+        } {redis {} bar1}
+
+        test "Redis can DEL key loaded from ssdb to redis with ttl($ttl)" {
+            $redis del foo
+            list [$redis locatekey foo] [$ssdb get foo] [$redis get foo]
+        } {none {} {}}
+
+        test "Key(become cold) move from redis to ssdb with ttl($ttl)" {
+            # TODO
+            if {$ttl > 0} {
+                $redis setex foo $ttl bar
+            } else {
+                $redis set foo bar
+            }
+            $redis dumptossdb foo
+
+            #wait key dumped to ssdb
+            wait_for_condition 100 1 {
+                [ $ssdb get foo ] eq {bar}
+            } else {
+                fail "key foo be dumptossdb failed"
+            }
+
+            $redis locatekey foo
+        } {ssdb}
+
+        test "Redis can DEL key stored in ssdb with ttl($ttl)" {
+            $redis del foo
+            list [$redis locatekey foo] [$ssdb get foo] [$redis get foo]
+        } {none {} {}}
+
+        test "SET new key(Hot key) not store in ssdb with ttl($ttl)" {
+            if {$ttl > 0} {
+                $redis setex foo $ttl bar
+            } else {
+                $redis set foo bar
+            }
+            list [$ssdb get foo] [$redis get foo]
+        } {{} bar}
+
+        test "GET key(Hot key) store in redis not operate ssdb with ttl($ttl)" {
+            set precalls [ get_total_calls "total_calls" $ssdb]
+            $redis get foo
+            set nowcalls [ get_total_calls "total_calls" $ssdb]
+            expr $nowcalls-$precalls
+        } 1
+
+        test "GET key(not exist) not operate ssdb with ttl($ttl)" {
+            set precalls [ get_total_calls "total_calls" $ssdb]
+            $redis get fooxxx
+            set nowcalls [ get_total_calls "total_calls" $ssdb]
+            expr $nowcalls-$precalls
+        } 1
         $redis del foo
-        
-        $redis set foo bar
-        list [$redis locatekey foo] [ $ssdb get foo ] [ $redis get foo ] 
-    } {redis {} bar}
 
-    test "Key(become cold) move from redis to ssdb" {
-        $redis dumptossdb foo
-        $redis set fooxxx barxxx
-
-        #wait key dumped to ssdb
-        wait_for_condition 100 1 {
-            [ $ssdb get foo ] eq {bar}
-        } else {
-            fail "key foo be dumptossdb failed"
+        if {$ttl > 0} {
+            test "key store in ssdb with ttl(5) will expire" {
+                $redis setex foo 5 bar
+                $redis dumptossdb foo
+                assert {[$redis locatekey foo] eq {ssdb}}
+                after 5100
+                list [$redis locatekey foo] [$redis get foo]
+            } {none {}}
         }
-
-        $redis bgsave
-        #wait bgsave complete
-        after 200
-        #start a test redis-server not connect to ssdb.
-        set nossdbpid [exec ../../../src/redis-server $nossdbcfgfile > /dev/null &]
-        after 1000
-        set redis_no_ssdb [redis 127.0.0.1 6389]
-        assert_equal "barxxx" [$redis_no_ssdb get fooxxx] "fooxxx stored in redis"
-        list [$redis locatekey foo] [ $redis_no_ssdb get foo ] [ $ssdb get foo ]
-    } {ssdb {} bar}
-
-    test "Redis can read key stored in ssdb" {
-        $redis get foo
-    } {bar}
-
-    test "GET Key(become hot) move from ssdb to redis" {
-        list [$redis locatekey foo] [ $ssdb get foo ]
-    } {redis {}}
-
-    test "Key(become cold) move from redis to ssdb" {
-        $redis dumptossdb foo
-
-        #wait key dumped to ssdb
-        wait_for_condition 100 1 {
-            [ $ssdb get foo ] eq {bar}
-        } else {
-            fail "key foo be dumptossdb failed"
-        }
-
-        $redis locatekey foo 
-    } {ssdb}
-
-    test "SET Key(become hot) move from ssdb to redis" {
-        $redis set foo bar1
-        list [$redis locatekey foo] [$ssdb get foo] [$redis get foo] 
-    } {redis {} bar1}
-
-    test "Redis can DEL key loaded from ssdb to redis" {
-        $redis del foo
-        list [$redis locatekey foo] [$ssdb get foo] [$redis get foo]
-    } {none {} {}}
-
-    test "Key(become cold) move from redis to ssdb" {
-        # TODO
-        $redis set foo bar
-        $redis dumptossdb foo
-
-        #wait key dumped to ssdb
-        wait_for_condition 100 1 {
-            [ $ssdb get foo ] eq {bar}
-        } else {
-            fail "key foo be dumptossdb failed"
-        }
-
-        $redis locatekey foo
-    } {ssdb}
-
-    test "Redis can DEL key stored in ssdb" {
-        $redis del foo
-        list [$redis locatekey foo] [$ssdb get foo] [$redis get foo]
-    } {none {} {}}
-
-    test "SET new key(Hot key) not store in ssdb" {
-        $redis set foo bar
-        list [$ssdb get foo] [$redis get foo]
-    } {{} bar}
-
-    test "GET key(Hot key) store in redis not operate ssdb" {
-        set precalls [ get_total_calls "total_calls" $ssdb]
-        $redis get foo
-        set nowcalls [ get_total_calls "total_calls" $ssdb]
-        expr $nowcalls-$precalls
-    } 1
-
-    test "GET key(not exist) not operate ssdb" {
-        set precalls [ get_total_calls "total_calls" $ssdb]
-        $redis get fooxxx
-        set nowcalls [ get_total_calls "total_calls" $ssdb]
-        expr $nowcalls-$precalls
-    } 1
-
+    }
     catch {exec kill -9 $nossdbpid}
     catch {exec kill -9 $ssdbpid}
 }
