147b520947c028685d33e6d7db6a33ad1083b9ac
zhaojun <motablow@gmail.com>
2017-03-17 11:48:08 +0800

    [Test] [SWAP-9] key转存到ssdb后lfu计数被重新初始化.

---
+++ redis-4.0-rc2/tests/qa/integration/tests/ssdb/lfu.tcl
@@ -0,0 +1,26 @@
+start_server {tags {"ssdb"}
+overrides {maxmemory 0}} {
+    test "new key's lfu is 5" {
+        r set foo bar
+        assert_equal 5 [r object freq foo] "[r object freq foo] not equal 5"
+        r del foo
+    }
+
+    test "object freq can count key's lfu in ssdb" {
+        r set foo bar
+        dumpto_ssdb_and_wait r foo
+        assert_equal 5 [r object freq foo] ""
+        r del foo
+    }
+
+    test "#issue lfu count be initialled to 5 when key dump to ssdb " {
+        for {set i 0} {$i < 1000} {incr i} {
+            r incr foo
+        }
+        set freq [r object freq foo]
+        assert {$freq > 5}
+        dumpto_ssdb_and_wait r foo
+        assert_equal $freq [r object freq foo] "ssdb freq:[r object freq foo] wrong"
+        r del foo
+    }
+}
