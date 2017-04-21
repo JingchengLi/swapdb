fcf51dc3262d65fb2dfe5f28fcd85a6c52caefd9
antirez <antirez@gmail.com>
2017-04-21 16:27:38 +0200

    Check event loop creation return value. Fix #3951.
    Normally we never check for OOM conditions inside Redis since the
    allocator will always return a pointer or abort the program on OOM
    conditons. However we cannot have control on epool_create(), that may
    fail for kernel OOM (according to the manual page) even if all the
    parameters are correct, so the function aeCreateEventLoop() may indeed
    return NULL and this condition must be checked.

--- redis-4.0-rc2/src/server.c
+++ redis-4.0-rc2/src/server.c
@@ -2542,7 +2542,13 @@ void initServer(void) {
     createSharedObjects();
     adjustOpenFilesLimit();
     server.el = aeCreateEventLoop(server.maxclients+CONFIG_FDSET_INCR);
-    server.db = zmalloc(sizeof(redisDb)* server.dbnum);
+    if (server.el == NULL) {
+        serverLog(LL_WARNING,
+            "Failed creating the event loop. Error message: '%s'",
+            strerror(errno));
+        exit(1);
+    }
+    server.db = zmalloc(sizeof(redisDb)*server.dbnum);
 
     /* Open the TCP listening socket for the user commands. */
     if (server.port != 0 &&
