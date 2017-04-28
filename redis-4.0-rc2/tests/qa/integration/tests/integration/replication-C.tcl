41d843108dd11b612ca4efb99a18cc3484ad2419
zhaojun <motablow@gmail.com>
2017-04-28 14:41:20 +0800

    [Test] add check all keys storetossdb when satisfy condition even when ssdb restart.

--- redis-4.0-rc2/tests/qa/integration/tests/integration/replication-C.tcl
+++ redis-4.0-rc2/tests/qa/integration/tests/integration/replication-C.tcl
@@ -31,10 +31,11 @@ start_server {tags {"repl-abnormal"}} {
                 fail "Different number of keys between master and slave after too long time."
             }
             wait_for_condition 100 100 {
-                [$master debug digest] == [[lindex $slaves 0] debug digest]
+                [$master debug digest] == [$slave debug digest]
             } else {
-                fail "Different digest between master([$master debug digest]) and slave1([[lindex $slaves 0] debug digest]) after too long time."
+                fail "Different digest between master([$master debug digest]) and slave([$slave debug digest]) after too long time."
             }
+            assert_equal "" [status $master db0] "No keys should stay in redis"
             compare_debug_digest
         }
     }
@@ -53,10 +54,10 @@ start_server {tags {"repl-abnormal"}} {
         set clist [ start_bg_complex_data_list $master_host $master_port $num $clients ]
         after 500
 
-        test "slave sync with master" {
-            $slave slaveof $master_host $master_port
-            wait_for_online $master
-        }
+     #   test "slave sync with master" {
+     #       $slave slaveof $master_host $master_port
+     #       wait_for_online $master
+     #   }
 
         test "master ssdb restart during writing" {
             after 500
@@ -65,16 +66,18 @@ start_server {tags {"repl-abnormal"}} {
             restart_ssdb_server -1
             set master [srv -1 client]
             stop_bg_client_list $clist
-            wait_for_condition 100 100 {
-                [$master dbsize] == [$slave dbsize]
-            } else {
-                fail "Different number of keys between master and slave after too long time."
-            }
-            wait_for_condition 100 100 {
-                [$master debug digest] == [$slave debug digest]
-            } else {
-                fail "Different digest between master([$master debug digest]) and slave([$slave debug digest]) after too long time."
-            }
+     #       wait_for_condition 100 100 {
+     #           [$master dbsize] == [$slave dbsize]
+     #       } else {
+     #           fail "Different number of keys between master and slave after too long time."
+     #       }
+     #       wait_for_condition 100 100 {
+     #           [$master debug digest] == [$slave debug digest]
+     #       } else {
+     #           fail "Different digest between master([$master debug digest]) and slave([$slave debug digest]) after too long time."
+     #       }
+            after 1000000
+            assert_equal "" [status $master db0] "No keys should stay in redis"
             compare_debug_digest
         }
     }
