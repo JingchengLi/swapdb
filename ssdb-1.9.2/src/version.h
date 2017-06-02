6e44e712898adeebca241a7858736445aabbd71e
zhangtianshuo <zts1993@gmail.com>
2017-06-02 16:44:27 +0800

    [...] ...
    Signed-off-by: zhangtianshuo <zhangtianshuo@jd.com>

--- ssdb-1.9.2/src/version.h
+++
@@ -1,8 +0,0 @@
-#ifndef SSDB_DEPS_H
-#ifndef SSDB_VERSION
-#define SSDB_VERSION "1.9.4"
-#endif
-#define GIT_BUILD "d6a28f852ac9f9be37b4bc2be0b410a7915e242f"
-#define BUILD_DATE "2017-06-02T15:34:26-CST"
-#endif
-#include <jemalloc/jemalloc.h>
