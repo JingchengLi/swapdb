48a3e92158e1c6c7f70ddf557a696d2ac7c228f7
zhaojun <motablow@gmail.com>
2017-07-07 18:03:46 +0800

    [Test] rename replcation-abc for windows case sensitive.

--- redis-4.0-rc2/tests/qa/integration/tests/unit/introspection-2.tcl
+++ redis-4.0-rc2/tests/qa/integration/tests/unit/introspection-2.tcl
@@ -1,6 +1,6 @@
 start_server {tags {"introspection"}
 overrides {lfu-decay-time 1}} {
-    test {TTL and TYPYE do not alter the lfu count of a key} {
+    test {TTL and TYPE do not alter the lfu count of a key} {
         ssdbr set foo bar
         ssdbr ttl foo
         ssdbr type foo
