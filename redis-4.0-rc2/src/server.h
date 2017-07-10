ce61a5121395fdc75573c44a0a6386f898457a1c
antirez <antirez@gmail.com>
2017-07-10 13:38:23 +0200

    AOF check utility: ability to check files with RDB preamble.

--- redis-4.0-rc2/src/server.h
+++ redis-4.0-rc2/src/server.h
@@ -2130,9 +2130,10 @@ void sentinelTimer(void);
 char *sentinelHandleConfiguration(char **argv, int argc);
 void sentinelIsRunning(void);
 
-/* redis-check-rdb */
-int redis_check_rdb(char *rdbfilename);
-int redis_check_rdb_main(int argc, char **argv);
+/* redis-check-rdb & aof */
+int redis_check_rdb(char *rdbfilename, FILE *fp);
+int redis_check_rdb_main(int argc, char **argv, FILE *fp);
+int redis_check_aof_main(int argc, char **argv);
 
 /* Scripting */
 void scriptingInit(int setup);
