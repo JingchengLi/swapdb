5b2cbb9f62f6168bb85fb91291b54cbab9b1ae53
zhangtianshuo <zts1993@gmail.com>
2017-08-11 17:38:22 +0800

    ..
    Signed-off-by: zhangtianshuo <zhangtianshuo@jd.com>

--- ssdb-1.9.2/src/net/resp.cpp
+++ ssdb-1.9.2/src/net/resp.cpp
@@ -59,41 +59,41 @@ void Response::add(const std::string &s){
 
 
 void Response::reply_ok() {
-	resp.push_back("ok");
+	resp.emplace_back("ok");
 }
 
 
 void Response::reply_errror(const std::string &errmsg) {
-	resp.push_back("error");
+	resp.emplace_back("error");
 	resp.push_back(errmsg);
 }
 
 
 void Response::reply_status(int status){
 	if(status == -1){
-		resp.push_back("error");
+		resp.emplace_back("error");
 	}else{
-		resp.push_back("ok");
+		resp.emplace_back("ok");
 	}
 }
 
 void Response::reply_bool(int status){
 	if(status == -1){
-		resp.push_back("error");
+		resp.emplace_back("error");
 	}else if(status == 0){
-		resp.push_back("ok");
-		resp.push_back("0");
+		resp.emplace_back("ok");
+		resp.emplace_back("0");
 	}else{
-		resp.push_back("ok");
-		resp.push_back("1");
+		resp.emplace_back("ok");
+		resp.emplace_back("1");
 	}
 }
 
 void Response::reply_int(int status, uint64_t val){
 	if(status == -1){
-		resp.push_back("error");
+		resp.emplace_back("error");
 	}else{
-		resp.push_back("ok");
+		resp.emplace_back("ok");
 		this->add(val);
 	}
 }
@@ -104,38 +104,38 @@ void Response::reply_int(int status, int val){
 
 void Response::reply_int(int status, int64_t val){
 	if(status == -1){
-		resp.push_back("error");
+		resp.emplace_back("error");
 	}else{
-		resp.push_back("ok");
+		resp.emplace_back("ok");
 		this->add(val);
 	}
 }
 
 void Response::reply_long_double(int status, long double val){
 	if(status == -1){
-		resp.push_back("error");
+		resp.emplace_back("error");
 	}else{
-		resp.push_back("ok");
+		resp.emplace_back("ok");
 		this->add(val);
 	}
 }
 
 void Response::reply_double(int status, double val){
 	if(status == -1){
-		resp.push_back("error");
+		resp.emplace_back("error");
 	}else{
-		resp.push_back("ok");
+		resp.emplace_back("ok");
 		this->add(val);
 	}
 }
 
 void Response::reply_get(int status, const std::string *val){
 	if(status < 0){
-		resp.push_back("error");
+		resp.emplace_back("error");
 	}else if(status == 0){
-		resp.push_back("not_found");
+		resp.emplace_back("not_found");
 	}else{
-		resp.push_back("ok");
+		resp.emplace_back("ok");
 		if(val){
 			resp.emplace_back(*val);
 		}
@@ -145,14 +145,14 @@ void Response::reply_get(int status, const std::string *val){
 
 void Response::reply_scan_ready() {
 	resp.clear();
-	resp.push_back("ok");
-	resp.push_back("0");
+	resp.emplace_back("ok");
+	resp.emplace_back("0");
 }
 
 void Response::reply_list_ready() {
-	resp.push_back("ok");
+	resp.emplace_back("ok");
 }
 
 void Response::reply_not_found() {
-	resp.push_back("not_found");
+	resp.emplace_back("not_found");
 }
