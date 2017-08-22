hanzoai/kv

HMSET and MSET implementations unified. HSET now variadic.
This is the first step towards getting rid of HMSET which is a command
that does not make much sense once HSET is variadic, and has a saner
return value.

src/server.c
@@ -144,7 +144,7 @@ struct redisCommand redisCommandTable[] = {
     {"substr",getrangeCommand,4,"r",0,NULL,1,1,1,0,0},
     {"incr",incrCommand,2,"wmF",0,NULL,1,1,1,0,0},
     {"decr",decrCommand,2,"wmF",0,NULL,1,1,1,0,0},
-    {"mget",mgetCommand,-2,"r",0,NULL,1,-1,1,0,0},
+    {"mget",mgetCommand,-2,"rF",0,NULL,1,-1,1,0,0},
     {"rpush",rpushCommand,-3,"wmF",0,NULL,1,1,1,0,0},
     {"lpush",lpushCommand,-3,"wmF",0,NULL,1,1,1,0,0},
     {"rpushx",rpushxCommand,-3,"wmF",0,NULL,1,1,1,0,0},
@@ -198,11 +198,11 @@ struct redisCommand redisCommandTable[] = {
     {"zrank",zrankCommand,3,"rF",0,NULL,1,1,1,0,0},
     {"zrevrank",zrevrankCommand,3,"rF",0,NULL,1,1,1,0,0},
     {"zscan",zscanCommand,-3,"rR",0,NULL,1,1,1,0,0},
-    {"hset",hsetCommand,4,"wmF",0,NULL,1,1,1,0,0},
+    {"hset",hsetCommand,-4,"wmF",0,NULL,1,1,1,0,0},
     {"hsetnx",hsetnxCommand,4,"wmF",0,NULL,1,1,1,0,0},
     {"hget",hgetCommand,3,"rF",0,NULL,1,1,1,0,0},
-    {"hmset",hmsetCommand,-4,"wm",0,NULL,1,1,1,0,0},
-    {"hmget",hmgetCommand,-3,"r",0,NULL,1,1,1,0,0},
+    {"hmset",hsetCommand,-4,"wmF",0,NULL,1,1,1,0,0},
+    {"hmget",hmgetCommand,-3,"rF",0,NULL,1,1,1,0,0},
     {"hincrby",hincrbyCommand,4,"wmF",0,NULL,1,1,1,0,0},
     {"hincrbyfloat",hincrbyfloatCommand,4,"wmF",0,NULL,1,1,1,0,0},
     {"hdel",hdelCommand,-3,"wF",0,NULL,1,1,1,0,0},

JingchengLi/swapdb

redis-4.0-rc2/src/server.c
@@ -155,7 +155,7 @@ struct redisCommand redisCommandTable[] = {
     {"incr",incrCommand,2,"wmFJ",0,NULL,1,1,1,0,0},
 #endif
     {"decr",decrCommand,2,"wmFJ",0,NULL,1,1,1,0,0},
-    {"mget",mgetCommand,-2,"r",0,NULL,1,-1,1,0,0},
+    {"mget",mgetCommand,-2,"rF",0,NULL,1,-1,1,0,0},
     {"rpush",rpushCommand,-3,"wmFJ",0,NULL,1,1,1,0,0},
     {"lpush",lpushCommand,-3,"wmFJ",0,NULL,1,1,1,0,0},
     {"rpushx",rpushxCommand,-3,"wmFJ",0,NULL,1,1,1,0,0},
@@ -209,11 +209,11 @@ struct redisCommand redisCommandTable[] = {
     {"zrank",zrankCommand,3,"rFJ",0,NULL,1,1,1,0,0},
     {"zrevrank",zrevrankCommand,3,"rFJ",0,NULL,1,1,1,0,0},
     {"zscan",zscanCommand,-3,"rR",0,NULL,1,1,1,0,0},
-    {"hset",hsetCommand,4,"wmFJ",0,NULL,1,1,1,0,0},
+    {"hset",hsetCommand,-4,"wmFJ",0,NULL,1,1,1,0,0},
     {"hsetnx",hsetnxCommand,4,"wmFJ",0,NULL,1,1,1,0,0},
     {"hget",hgetCommand,3,"rFJ",0,NULL,1,1,1,0,0},
-    {"hmset",hmsetCommand,-4,"wmJ",0,NULL,1,1,1,0,0},
-    {"hmget",hmgetCommand,-3,"rJ",0,NULL,1,1,1,0,0},
+    {"hmset",hsetCommand,-4,"wmFJ",0,NULL,1,1,1,0,0},
+    {"hmget",hmgetCommand,-3,"rFJ",0,NULL,1,1,1,0,0},
     {"hincrby",hincrbyCommand,4,"wmFJ",0,NULL,1,1,1,0,0},
     {"hincrbyfloat",hincrbyfloatCommand,4,"wmFJ",0,NULL,1,1,1,0,0},
     {"hdel",hdelCommand,-3,"wFJ",0,NULL,1,1,1,0,0},