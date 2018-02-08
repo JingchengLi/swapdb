66efd9f919dfaa56dcefd9b39a8bdabe57624546
zhaojun11 <zhaojun11@jd.com>
2018-02-08 11:40:34 +0800

    Optimize deploy port and colorize info

--- utils/deploy_redis.sh
+++ utils/deploy_redis.sh
@@ -5,10 +5,24 @@ REDIS_CFG=redis.conf.template
 SSDB=../../../build/ssdb-server
 REDIS=../../../build/redis-server
 
+ERROR(){
+    echo -e "\033[37;41m[ERROR]\033[0m $@"
+}
+
+INFO(){
+    echo -e "\033[37;42m[INFO]\033[0m $@"
+}
+
 if [ -n "$1" ];then
     REDISPORT=$1
 else
     REDISPORT=6379
+fi
+let SSDBPORT=($REDISPORT+20000)%65535
+
+if [[ `lsof -i:$REDISPORT` > 1 || `lsof -i:$SSDBPORT` > 1 ]];then
+    ERROR "redis port:$REDISPORT or ssdb port:$SSDBPORT is already used!"
+    exit 1
 fi
 
 DIR=./tmp/${REDISPORT}_dir
@@ -20,20 +34,21 @@ prepare_dir() {
 
 startSSDBServer(){
     if [[ ! -f "$SSDB" ]]; then
-        echo "$SSDB not exists, must build it at first!"
-        exit 0
+        ERROR "$SSDB not exists, must build it at first!"
+        exit 1
     fi
-    sed -i "s/6379/$REDISPORT/g" $SSDB_CFG
+    sed -i "s/ssdbport/$SSDBPORT/g" $SSDB_CFG
+    sed -i "s/redisport/$REDISPORT/g" $SSDB_CFG
     mv $SSDB_CFG ${SSDB_CFG}_${REDISPORT}
     $SSDB -d ${SSDB_CFG}_${REDISPORT} -s restart &> /dev/null
 }
 
 startRedisServer(){
     if [[ ! -f "$REDIS" ]]; then
-        echo "$REDIS not exists, must build it at first!"
-        exit 0
+        ERROR "$REDIS not exists, must build it at first!"
+        exit 1
     fi
-    sed -i "s/port [0-9]\{1,\}/port $REDISPORT/g"  $REDIS_CFG
+    sed -i "s/redisport/$REDISPORT/g"  $REDIS_CFG
     $REDIS $REDIS_CFG &> /dev/null &
 }
 
@@ -43,13 +58,13 @@ startSSDBServer
 startRedisServer
 
 if [ "" == "`ps -ef | grep ssdb-server | grep "_$REDISPORT"`" ]; then
-    echo "ssdb-server at port $REDISPORT not start!"
-    exit 0
+    ERROR "ssdb-server at port $REDISPORT not start!"
+    exit 1
 fi
 
 if [ "" == "`ps -ef | grep redis-server | grep ":$REDISPORT"`" ]; then
-    echo "redis-server at port $REDISPORT not start!"
-    exit 0
+    ERROR "redis-server at port $REDISPORT not start!"
+    exit 1
 fi
 
-echo "Start SWAP server at port $REDISPORT success!"
+INFO "Start SWAP server at port $REDISPORT success!"
